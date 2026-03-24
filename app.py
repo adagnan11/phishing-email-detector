d7d4005d44a4afad04338171e7968756f8616fd7
Arthur Dagnan <adagnan@neiu.edu>
2026-03-24 17:12:39 -0500

    Refactor app.py for improved email prediction logic

--- app.py
+++ app.py
@@ -82,6 +82,7 @@ def index():
 
     except Exception as e:
         return f"ERROR: {str(e)}"
-        if __name__ == "__main__":
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
